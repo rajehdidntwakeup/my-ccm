f51695a9d48d09954e7b75ac32ead2c4083be761
rajehdidntwakeup <84966465+rajehdidntwakeup@users.noreply.github.com>
2024-02-24 15:32:55 +0100

    creating new project with the basic dependencies (#2)
    Co-authored-by: Rajeh Abdulhadi <Rajeh Abdulhadi@DESKTOP-M4UCA7S.home>

---
+++ src/main/java/project/cmm/myccm/MyCcmApplication.java
@@ -0,0 +1,13 @@
+package project.cmm.myccm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/cmm/myccm/MyCcmApplicationTests.java
@@ -0,0 +1,13 @@
+package project.cmm.myccm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
